3b6b1c933f3431b83c642300815cacf0c2581983
thanhdat <lethanhdat.real@gmail.com>
2024-02-26 22:48:27 +0700

    feat: add export to csv file

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const puppeteer = require('puppeteer')
 const crawData = require('./controllers/crawData')
 const arrCalendar = require('./controllers/convertToCalendarTemplate')
+const arrToCSV = require('./controllers/arrToCSV')
 const fs = require('fs')
 const app = express()
 
@@ -11,18 +12,9 @@ const PORT = process.env.PORT || 5000
 app.get('/', (req, res) => {
     (async () => {
         const content = await crawData()
-        const csvData = await arrCalendar(content)
-
-        arrayToCsv = (arr, filePath) => {
-            // Convert the array to a CSV string
-            const csvString = arr.map(row => row.join(',')).join('\n');
-            // Write the CSV string to a file
-            fs.writeFileSync(filePath, csvString, 'utf-8');
-        }
-
-        arrayToCsv(csvData, 'output.csv');
-
-        await res.send(csvData)
+        const arrData = await arrCalendar(content)
+        const exportToCSV = await arrToCSV(arrData, 'output.csv')
+        await res.send(arrData)
     })()
 })
 
