1372eb080ce532bc369e078b5d1b7ca3b732b8c4
thanhdat <lethanhdat.real@gmail.com>
2024-05-17 12:12:11 +0700

    init project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import Table from "./components/Table"
-
+// import Table from "./components/Table"
+import MyCalendar from './components/Calendar'
+import { useState } from 'react';
 function App() {
 
   return (
     <>
-      <div className="row">
-        <div className="col">
-          <p>
-            B1: Get cookies from dt-ctt.sis.hust.edu.vn <br />
-            B2: Fill cookies in form down below <br />
-            B3: Export calendar <br />
-          </p>
-        </div>
-
-        <div className="col">
-          <Table api='http://localhost:3000/books' />
-        </div>
+      {/* <div className="row"> */}
+      <div className="">
+        <p>
+          B1: Get cookies from dt-ctt.sis.hust.edu.vn <br />
+          B2: Fill cookies in form down below <br />
+          B3: Export calendar <br />
+        </p>
       </div>
+
+      <div className="">
+        <h1>My Calendar App</h1>
+        <MyCalendar />
+      </div>
+      {/* </div> */}
     </>
   )
 }
