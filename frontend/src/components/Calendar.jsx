cc108f994d58008332cfec15015ab41fce26edcd
thanhdat <lethanhdat.real@gmail.com>
2024-05-17 17:55:01 +0700

    update calendar view

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,6 +1,5 @@
 // Calendar.js
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -8,29 +7,16 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 // Thiết lập localizer sử dụng moment
 const localizer = momentLocalizer(moment);
 
-const MyCalendar = () => {
-    const [events, setEvents] = useState([{}])
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await axios.get('http://localhost:3000/event-calendar')
-                console.log(res.data)
-                setEvents(res.data)
-            }
-            catch (err) {
-                console.log(err)
-                res.send("err")
-            }
-        }
-        fetchData()
-    }, [])
+const MyCalendar = ({ events }) => {
 
     return (
-        <div style={{ height: 500 }}>
+        <div>
             <Calendar
                 localizer={localizer}
                 events={events}
-                style={{ height: 500 }}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 1000 }}
             />
         </div>
     );
