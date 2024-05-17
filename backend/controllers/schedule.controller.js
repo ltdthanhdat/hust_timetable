e2b6b77cf7b98fd9bd9a77cafcc12acc68484282
thanhdat <lethanhdat.real@gmail.com>
2024-05-17 17:51:47 +0700

    update controller

--- backend/controllers/schedule.controller.js
+++ backend/controllers/schedule.controller.js
@@ -1,21 +1,30 @@
+import fs from 'fs'
 import crawData from '../utils/crawData.js'
 import convertToCsv from '../utils/convertToCsv.js'
 import convertToEvent from '../utils/convertToEvent.js'
 const htmlTable = async (req, res) => {
-    const data = await crawData()
+    // console.log(req.body.aspNetCookies)
+    const data = await crawData(req.body.aspNetCookies)
     res.send(data)
 }
 
 const eventCalendar = async (req, res) => {
-    const rawData = await crawData()
+    const rawData = await crawData(req.body.aspNetCookies)
     const data = convertToCsv(rawData)
     res.send(convertToEvent(data))
 }
 
 const csvTemplate = async (req, res) => {
-    const data = await crawData()
+    const data = await crawData(req.body.aspNetCookies)
     res.send(convertToCsv(data))
 }
 
+const exportCsv = async (req, res) => {
+    const rawData = await crawData(req.body.aspNetCookies)
+    const data = convertToCsv(rawData)
+    const csvString = data.map(row => row.join(',')).join('\n');
+    fs.writeFileSync("output.csv", csvString, 'utf-8');
+    res.send("ok")
+}
 
-export default { htmlTable, eventCalendar, csvTemplate }
+export default { htmlTable, eventCalendar, csvTemplate, exportCsv }
