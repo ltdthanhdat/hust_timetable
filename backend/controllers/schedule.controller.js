dc8bf3ffc9c460ae5afb16048a688f4fd4ef500e
thanhdat <lethanhdat.real@gmail.com>
2024-09-14 17:40:15 +0700

    remove api for testing

--- backend/controllers/schedule.controller.js
+++ backend/controllers/schedule.controller.js
@@ -3,26 +3,15 @@ import crawData from '../utils/crawData.js'
 import convertToCsv from '../utils/convertToCsv.js'
 import convertToEvent from '../utils/convertToEvent.js'
 
-//const htmlTable = async (req, res) => {
-//	const data = await crawData(req.body.aspNetCookies.trim())
-//	res.send(data)
-//}
-
 const eventCalendar = async (req, res) => {
 	const rawData = await crawData(req.body.aspNetCookies.trim())
 	const data = convertToCsv(rawData)
 	res.send(convertToEvent(data))
 }
 
-//const csvTemplate = async (req, res) => {
-//	const data = await crawData(req.body.aspNetCookies.trim())
-//	res.send(convertToCsv(data))
-//}
-
 const exportCsv = async (req, res) => {
 	const rawData = await crawData(req.body.aspNetCookies.trim())
 	const data = convertToCsv(rawData)
-	// console.log(data)
 	if (data == '') {
 		fs.writeFileSync("output.csv", '', 'utf-8');
 		res.send('no data')
@@ -34,5 +23,4 @@ const exportCsv = async (req, res) => {
 	}
 }
 
-//export default { htmlTable, eventCalendar, csvTemplate, exportCsv }
 export default { eventCalendar, exportCsv }
